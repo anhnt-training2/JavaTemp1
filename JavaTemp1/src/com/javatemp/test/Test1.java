d6f8a56002839cc9b36af7dfe8a8611a921bdf2a
anhnt <anhnt@DESKTOP-N5IUTJO>
2016-10-28 18:19:50 +0700

    this is test

--- JavaTemp1/src/com/javatemp/test/Test1.java
+++ JavaTemp1/src/com/javatemp/test/Test1.java
@@ -5,6 +5,14 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("JavaTemp Hello");
+		System.out.println("This is test");
+		System.out.println("This is test");
+
+		System.out.println("This is test");
+		System.out.println("This is test");
+
+		System.out.println("This is test");
+		
 	}
 
 }
