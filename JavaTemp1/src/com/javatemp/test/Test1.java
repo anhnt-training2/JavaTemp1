e931d4f0b88d47882ca1688c07440e066d7f668a
anhnt <anhnt@DESKTOP-N5IUTJO>
2016-10-28 17:56:36 +0700

    init javatemp project 1

---
+++ JavaTemp1/src/com/javatemp/test/Test1.java
@@ -0,0 +1,10 @@
+package com.javatemp.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JavaTemp Hello");
+	}
+
+}
